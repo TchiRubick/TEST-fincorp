fe773a19b27b2d5c0d6f625695641911d27aaf6f
Tchi <tchi.devica@gmail.com>
2021-08-16 22:58:32 +0200

    feat: details cover

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,11 +3,15 @@ import React from 'react';
 import './Home.scss';
 
 import CoverSection from '../../components/sections/cover/CoverSection';
+import DetailsSection from '../../components/sections/details/DetailsSection';
 
 const Home = () => (
   <div className="page-home">
     <div className="page-home__cover">
       <CoverSection />
+    </div>
+    <div className="page-home__details">
+      <DetailsSection />
     </div>
     Page Home
   </div>
